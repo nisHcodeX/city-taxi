5054fb2720e978b6f4d3181170b7c854f8bf3d5f
Shalin <nirvan.work@gmail.com>
2024-07-27 21:55:09 +0530

    Initialize project

---
+++ src/main/java/com/citytaxi/city_taxi/CityTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.citytaxi.city_taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityTaxiApplication.class, args);
+	}
+
+}
