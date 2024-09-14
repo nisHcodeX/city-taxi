8eaee17643026bae304f173d08366977e02acace
Shalin <nirvan.work@gmail.com>
2024-09-15 01:44:15 +0530

    Add cors config

---
+++ src/main/java/com/citytaxi/city_taxi/configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.citytaxi.city_taxi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry register) {
+        register.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                //.allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
